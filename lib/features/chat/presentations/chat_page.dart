41475c9924ad38ee65f1ecbd9b69e1d7112fe56d
Leon Chan <leonchantinglok@gmail.com>
2026-03-05 11:34:37 +0800

    feat: app bar UI

--- lib/features/chat/presentations/chat_page.dart
+++ lib/features/chat/presentations/chat_page.dart
@@ -6,7 +6,24 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Chat Page')),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.history),
+          tooltip: 'ChatHistory',
+          onPressed: () {
+            //TODO: Implement the logic for ChatHistory
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              //TODO: Implement the logic for Sign in
+            },
+            child: Text('Sign in'),
+          ),
+        ],
+      ),
       body: Center(child: Text('This is the Chat Page')),
     );
   }
