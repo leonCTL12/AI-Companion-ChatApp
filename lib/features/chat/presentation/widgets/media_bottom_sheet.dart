40ba224f41a8ddcebd8fe1797fe88ec3f64a20a4
Leon Chan <leonchantinglok@gmail.com>
2026-05-05 11:17:14 +0800

    feat: remove file from media bottom sheet

--- lib/features/chat/presentation/widgets/media_bottom_sheet.dart
+++ lib/features/chat/presentation/widgets/media_bottom_sheet.dart
@@ -53,26 +53,6 @@ class MediaBottomSheet extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(width: 8),
-          Expanded(
-            child: ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 16,
-                ),
-              ),
-              onPressed: () {},
-              icon: const Icon(Icons.insert_drive_file),
-              label: FittedBox(
-                fit: BoxFit.scaleDown,
-                child: const Text("Files"),
-              ),
-            ),
-          ),
         ],
       ),
     );
